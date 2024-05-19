b6ba4952e63edc291f4764fdca89dd5d3fec0635
jayudoye <joeludoye@gmail.com>
2024-05-19 16:55:15 -0700

    More Classes

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the States."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Public class attribute:
+    name (str): The name of the state.
+    """
+
+    name = ""
